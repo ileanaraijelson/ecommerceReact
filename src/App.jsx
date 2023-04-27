b7a12f76c98c85da0120600547ac8905d627ff09
ileana raijelson <ileraijelson@gmail.com>
2023-04-27 15:30:44 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
-import './App.css';
+import './styles.css';
+import {ItemListContainer} from './components/ItemListContainer';
 import second, { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+  <>
   <NavBar />
+  <ItemListContainer texto='3'/>
+  </>
   );
 }
 
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import second, { CartWidget } from "../CartWidget";
+import{Container, Nav, Navbar, NavDropdown, Form, Button}from'react-bootstrap'
+
 
 export const NavBar = () => {
     return(
-        <div className="container">
-            <nav className="nav">
-                <div className="nav__brand">
-                    <a className="nav__link" href="#">MiMarca</a>
-                </div>
-                    <ul className="nav__list">
-                        <li>
-                            <a className="nav__link" href="#">categoria 1</a>
-                        </li>
-                        <li>
-                            <a className="nav__link" href="#">categoria 2</a>
-                        </li>
-                        <li>
-                            <a className="nav__link" href="#">categoria 3</a>
-                        </li>
-                    </ul>
-            </nav>
-        </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Mundo Electronico</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Productos">Productos</Nav.Link>
+            <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#cartwidget"><CartWidget /> 1</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 }
+
